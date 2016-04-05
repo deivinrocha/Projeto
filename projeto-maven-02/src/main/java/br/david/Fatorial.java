8d8946caaa73317f82a8275c9ab707227961067e
David <deivin503@gmail.com>
2016-04-05 00:46:57 -0300

    Maven
    Adicionando projeto feito no maven.

---
+++ projeto-maven-02/src/main/java/br/david/Fatorial.java
@@ -0,0 +1,15 @@
+package br.david;
+
+public class Fatorial {
+	
+	public static int calcular(int x) {
+		int resultado = 0;
+		
+		if(x == 0 || x == 1) 
+			return 1;
+		
+		return resultado;
+	
+	}
+
+}
---
+++ projeto-maven-02/src/test/java/br/david/FatorialTest.java
@@ -0,0 +1,21 @@
+package br.david;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class FatorialTest {
+	@Test
+	public void testZero() {
+		int resultado = Fatorial.calcular(0);
+		assertEquals(resultado, 1, 0);
+	}
+
+	@Test
+	public void testUm() {
+		int resultado = Fatorial.calcular(1);
+		assertEquals(resultado, 1, 0.0);
+	}
+
+}
